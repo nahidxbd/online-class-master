bd7f1c5c47122dca365c96f92250963a7a364651
nahidxbd <nahidxbd87@gmail..com>
2020-08-28 02:43:21 +0600

    All done

---
+++ src/fakeData/fakeCourse.js
@@ -0,0 +1,83 @@
+var courses =[
+    {
+        "id": 0,
+        name: "Complete Guitar System - Beginner to Advanced",
+        img: "https://imgur.com/WZ090Cf.png",
+        price: 50
+    },
+    {
+        "id": 1,
+        name: "The Professional Guitar Masterclass",
+       img:"https://imgur.com/hRQVKTV.png",
+        price: 200
+    },
+    {
+        "id": 2,
+        name: "Ultimate Beginner Guitar Masterclass (2020 Update)",
+        img:"https://imgur.com/tNsPHSw.png",
+        price: 80
+    },
+    {
+        "id": 3,
+        name: "Master Guitar In 90 Days: Step-By-Step Lessons For Beginners",
+        img:"https://imgur.com/shgDyys.png",
+        price: 100
+    },
+    {
+        "id": 4,
+        name: "The Ultimate Guitar Tone School Course",
+        img:"https://imgur.com/pcubCat.png",
+        price: 300
+    },
+    {
+        "id": 5,
+        name: "Play YOUR Favorite Songs: The Guitar Super-Course",
+        img:"https://imgur.com/S4oGw5G.png",
+        price: 70
+    },
+    {
+        "id": 6,
+        name: "STRUMMING SIMPLIFIED: 51 Guitar Rhythms For All Styles!",
+        img:"https://imgur.com/8Q64Any.png",
+        price: 200
+    },
+    
+    {
+        "id": 7,
+        name: "Jazz for the Curious Guitarist",
+        img:"https://ichef.bbci.co.uk/images/ic/976xn/p05dnb1r.jpg",
+        price: 110
+    },
+    {
+        "id": 8,
+        name:"Complete Guitar System - Beginner to Advanced",
+        img:"https://imgur.com/3sUWYRf.png",
+        price: 500
+    },
+    {
+        "id": 9,
+        name: "Fingerstyle Focus - Learn to play Fingerstyle Guitar!",
+        img:"https://imgur.com/rTPL0A0.png",
+        price:150
+    },
+    {
+        "id": 10,
+        name: "Essential Theory and Technique for Guitar",
+        img:"https://imgur.com/k3gyRhm.png",
+        price: 40
+    },
+    {
+        "id": 11,
+        name: "Rockabilly Guitar For Beginners",
+        img:"https://imgur.com/Gqa2MLe.png",
+        price: 110
+    },
+    {
+        "id": 12,
+        name: "Learn guitar the Right way! Beginner to intermediate guitar",
+        img:"https://imgur.com/eEttO03.png",
+        price: 200
+    },
+    
+]
+export default courses;
